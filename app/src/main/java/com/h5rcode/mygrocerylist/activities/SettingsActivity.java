f7bf47a831464176a9c8bcab90d3f3aeb4019ad3
h5rcode <h5rcode@gmail.com>
2017-04-06 19:20:20 +0100

    Added a back button to the action bar in the settings activity.

--- app/src/main/java/com/h5rcode/mygrocerylist/activities/SettingsActivity.java
+++ app/src/main/java/com/h5rcode/mygrocerylist/activities/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.h5rcode.mygrocerylist.activities;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.h5rcode.mygrocerylist.fragments.settings.SettingsFragment;
 
@@ -14,5 +16,23 @@ public class SettingsActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
